0a89c12ab028f1f7b67514c8de9cb49be48f7d96
Patipat Sahasapanan <patipat.saha@gmail.com>
2018-08-28 18:09:12 +0700

    create new project cme viper

---
+++ CME Viper iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CME Viper iOS
+//
+//  Created by Patipat Sahasapanan on 28/8/2561 BE.
+//  Copyright © 2561 CreativeMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
